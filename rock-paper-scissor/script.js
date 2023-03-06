cc8475208ed300b7bddc7e8f084ab0a8f55de894
douglas <douglas.df1@gmail.com>
2023-03-06 19:55:31 -0300

    rock-paper-scissor

---
+++ rock-paper-scissor/script.js
@@ -0,0 +1,38 @@
+const options = ["rock", "paper", "scissors"]
+
+function getComputerChoice () {
+    return options[Math.floor(Math.random() * 3)];
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie";
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function getPlayerChoice(options) {
+    while (true) {
+        let playerChoice = prompt("Type rock, paper or scissors");
+        playerChoice = playerChoice.toLowerCase();
+        if (options.includes(playerChoice)) {
+            return playerChoice
+        } else {
+            continue
+        }
+    }
+}
+
+
+function game () {
+    for (i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice(options);
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
